6a2d2dce1ac5b22e9d070dc57ed6cce05d781394
Dilara Tekin <dilaratekkinn@gmail.com>
2024-09-26 13:18:03 +0300

    created data tables

---
+++ database/migrations/2024_09_26_090036_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('subtitle')->nullable();
+            $table->integer('author_id');
+            $table->longText('summary');
+            $table->text('cover_image');
+            $table->integer('category_id');
+            $table->integer('library_id');
+            $table->date('published_year');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
